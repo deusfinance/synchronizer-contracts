a4d893bd474625fce501468bd6df6bd7717aee7d
V-Coder <vahidgh.vgh@gmail.com>
2022-02-16 18:53:20 +0330

    Edit files name

--- scripts/deploy-crypto-and-forex.js
+++ scripts/deploy-crypto-and-forex.js
@@ -1,30 +1,40 @@
+const conductData = require("./data/crypto-forex.json")
+const { sleep } = require("./helpers/sleep")
 const { constructName, constructSymbol, constructVersion } = require("./helpers/naming")
 
 async function main() {
-  // configuration
   const conductorAddress = "0x570D710d9F20599551246EC24D8A8cCffeb57Ccf" // fantom
   const conductorInstance = await hre.ethers.getContractFactory("Conductor")
   const conductor = conductorInstance.attach(conductorAddress)
 
-  await conductor.conduct(
-    "BTC",
-    constructName('Bitcoin', "SHORT"),
-    constructSymbol('BTC', "SHORT"),
-    constructName("Bitcoin", "LONG"),
-    constructSymbol("BTC", "LONG"),
-    constructVersion(1, 0),
-    "1"
-  )
-
-  await conductor.conduct(
-    "XAU",
-    constructName('Gold', "SHORT"),
-    constructSymbol('XAU', "SHORT"),
-    constructName("Gold", "LONG"),
-    constructSymbol("XAU", "LONG"),
-    constructVersion(1, 0),
-    "2"
-  )
+  // for (let i = 0; i < 3; i++) {
+  //   console.log("*", conductData[i].symbol)
+  //   const tx = await conductor.conduct(
+  //     conductData[i].symbol,
+  //     constructName(conductData[i].name, "SHORT"),
+  //     constructSymbol(conductData[i].symbol, "SHORT"),
+  //     constructName(conductData[i].name, "LONG"),
+  //     constructSymbol(conductData[i].symbol, "LONG"),
+  //     constructVersion(1, 0),
+  //     "1"
+  //   )
+  //   // console.log(tx)
+  //   await sleep(10000)
+  // }
+  for (let i = 8; i < conductData.length; i++) {
+    console.log("*", conductData[i].symbol)
+    const tx = await conductor.conduct(
+      conductData[i].symbol,
+      constructName(conductData[i].name, "SHORT"),
+      constructSymbol(conductData[i].symbol, "SHORT"),
+      constructName(conductData[i].name, "LONG"),
+      constructSymbol(conductData[i].symbol, "LONG"),
+      constructVersion(1, 0),
+      "2"
+    )
+    // console.log(tx)
+    await sleep(10000)
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
